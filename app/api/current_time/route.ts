c3de0015837820f55e9a6a8be1bd091cee72ec1a
idriss <idriss.said@epitech.eu>
2024-05-01 11:48:02 +0400

    fix compilation

--- app/api/current_time/route.ts
+++ app/api/current_time/route.ts
@@ -1,8 +1,6 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { NextResponse } from "next/server";
-import { json } from "stream/consumers";
+import { NextRequest, NextResponse } from "next/server";
 
-export function GET(req: NextApiRequest, res: NextApiResponse) {
+export function GET(req: NextRequest, res: NextResponse) {
   const currentTime = Math.floor(Date.now() / 1000);
   return NextResponse.json({ data: currentTime }, { status: 200 });
 }
