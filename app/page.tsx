c04a0a77a497e27ee791dfdca91cb61409d66f35
idriss <idriss.said@epitech.eu>
2024-06-03 00:49:10 +0400

    set router in useEffect

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 'use client'
 
 import { useRouter } from "next/navigation"
+import { useEffect } from "react"
 
 const Page = () => {
   const router = useRouter()
-  router.push('/dashboard')
+  useEffect(() => {
+    router.replace('/dashboard')
+  })
   return <></>
 }
 
