dd384e832a110c950a2a8076b182a9585f84e741
idriss <idriss.said@epitech.eu>
2024-06-03 00:52:49 +0400

    try to add host

--- app/api/host/route.ts
+++ app/api/host/route.ts
@@ -2,7 +2,6 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest, res: NextResponse) {
   try {
-    return NextResponse.json({ data: "ipAddress" }, { status: 200 });
     const ipAddress = req?.headers?.get('x-forwarded-host');
     return NextResponse.json({ data: ipAddress }, { status: 200 });
   } catch (error) {
